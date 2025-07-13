196acc05f80454fefc9da347f2a05d10d5816025
devLakunle <devlakunle@gamil.com>
2025-07-13 11:58:40 +0100

    feat(events): add events list page
    - Create events list page for displaying school events data
    - Set up directory structure for events data management

---
+++ src/app/(dashboard)/list/events/page.tsx
@@ -0,0 +1,89 @@
+import Pagination from "@/components/Pagination";
+import Search from "@/components/Search";
+import Table from "@/components/Table";
+import Image from "next/image";
+import Link from "next/link";
+import { role, eventsData } from "../../../../../lib/Data";
+
+interface EventProp {
+  id: number;
+  title: string;
+  class: string;
+  date: string;
+  startTime: string;
+  endTime: number;
+}
+
+const columns = [
+  { header: "Title", accessor: "title" },
+  { header: "Class", accessor: "class" },
+  { header: "Date", accessor: "date", className: "hidden md:table-cell" },
+  {
+    header: "Start Time",
+    accessor: "startTime",
+    className: "hidden md:table-cell",
+  },
+  { header: "Action", accessor: "action" },
+];
+
+const ExamListPage = () => {
+  const renderCell = (items: EventProp) => (
+    <tr
+      key={items.id}
+      className="border-b border-gray-200 even:bg-slate-50 text-xs hover:bg-schoolPurpleLight xl:text-sm"
+    >
+      <td className="flex items-center p-4 gap-4">{items.title}</td>
+      <td>{items.class}</td>
+      <td className="hidden md:table-cell">{items.date}</td>
+      <td className="hidden md:table-cell">{items.startTime}</td>
+      <td className="hidden md:table-cell">{items.endTime}</td>
+      <td>
+        <div className="flex items-center gap-2">
+          <Link href={`/list/teachers/${items.id}`}>
+            <button className="w-8 h-8 p-2 rounded-full bg-schoolSky cursor-pointer">
+              <Image src="/edit.png" alt="" width={16} height={16} />
+            </button>
+          </Link>
+          {role === "admin" && (
+            <button className="w-8 h-8 p-2 rounded-full bg-schoolPurple cursor-pointer">
+              <Image src="/delete.png" alt="" width={16} height={16} />
+            </button>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+  return (
+    <div className="flex-1 mx-4 p-4 bg-white rounded-lg space-y-6 ">
+      {/* TOP  */}
+      <div className="center-btw gap-4 ">
+        <h1 className=" hidden md:block text-xl font-semibold ">All Events</h1>
+        <div className="flex items-center md:flex-row flex-col gap-4 w-full md:w-auto">
+          <Search />
+          <div className="flex items-center self-end md:self-center gap-4">
+            <button className="w-8 h-8 center rounded-full bg-schoolYellow p-2 cursor-pointer">
+              <Image
+                src="/filter.png"
+                alt="filter-logo"
+                width={20}
+                height={20}
+              />
+            </button>
+            <button className="w-8 h-8 center rounded-full bg-schoolYellow p-2 cursor-pointer">
+              <Image src="/sort.png" alt="sort-logo" width={20} height={20} />
+            </button>
+            <button className="w-8 h-8 center rounded-full bg-schoolYellow p-2 cursor-pointer">
+              <Image src="/plus.png" alt="plus-logo" width={20} height={20} />
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* CONTENT */}
+      <Table columns={columns} renderCell={renderCell} data={eventsData} />
+      {/* PAGINATION */}
+      <Pagination />
+    </div>
+  );
+};
+
+export default ExamListPage;
